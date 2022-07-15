0794de7cc38d757f2a0fbeeb15cbeccf6d0def20
emily-nicole-aguiar <106185441+emily-nicole-aguiar@users.noreply.github.com>
2022-07-15 16:58:16 -0400

    Update jogodaforca.py

--- jogodaforca.py
+++ jogodaforca.py
@@ -4,8 +4,27 @@ def jogar():
     print("             JOGO DA FORCA              ")
     print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
 
-    print("           EM BREVE !!!!!!!!!!!")
+    palavra_secreta ="caramelo"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_", "_"]
+    enforcou = False
+    acertou = False
+    print(letras_acertadas)
+    while (not acertou and not enforcou):
+        palpite = input("Digite um letra: ")
+        palpite = palpite.strip()
+        print("Jogando...")
 
+        index = 0
+
+        for letra in palavra_secreta:
+
+            if (palpite.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_faltando = str(letras_acertadas.count("_"))
+                print("Ainda faltam {} letras".format(letras_faltando))
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
 
     print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
     print("              FIM DO JOGO               ")
